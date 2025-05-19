82deb644a0286037f01e57e39e0f75c564bd9292
Elena <e.nowitskaya2017@yandex.ru>
2025-05-19 13:56:20 +0300

    настройка базового оформления

---
+++ site/js/Header.js
@@ -0,0 +1,17 @@
+document.write("<header id=\"my_header\" class=\"p-3 bg-dark text-white\">\n" +
+  "  <div class=\"container\">\n" +
+  "    <div class=\"d-flex flex-wrap justify-content-md-between justify-content-center\">\n" +
+  "      <ul class=\"col-md-12 justify-content-center nav nav-pills\">\n" +
+  "<li class=\"nav-item\"><a href=\"#\" onmouseenter=\"this.classList.add('active')\"\n" +
+  "                                onmouseleave=\"this.classList.remove('active')\" class=\"header-button nav-link\"\n" +
+  "                                aria-current=\"page\">Главная</a></li>\n" +
+  "        <li class=\"nav-item\"><a href=\"#\" onmouseenter=\"this.classList.add('active')\"\n" +
+  "                                onmouseleave=\"this.classList.remove('active')\" class=\"header-button nav-link\"\n" +
+  "                                aria-current=\"page\">Участники</a></li>\n" +
+  "        <li class=\"nav-item\"><a href=\"#\" onmouseenter=\"this.classList.add('active')\"\n" +
+  "                                onmouseleave=\"this.classList.remove('active')\" class=\"header-button nav-link\"\n" +
+  "                                aria-current=\"page\">Журнал</a></li>\n" +
+  "      </ul>\n" +
+  "    </div>\n" +
+  "  </div>\n" +
+  "</header>")
