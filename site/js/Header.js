9fbb905ed440e9f13a78d51aecbb24c748e47d5b
Elena <e.nowitskaya2017@yandex.ru>
2025-05-19 23:05:55 +0300

    настройка переходов

--- site/js/Header.js
+++ site/js/Header.js
@@ -9,11 +9,11 @@ document.write("<header class=\"sticky-top p-3 bg-dark text-white\">\n" +
   "  <div class=\"container\">\n" +
   "        <div class=\"d-flex flex-wrap justify-content-md-between justify-content-center\">\n" +
   "          <ul class=\"col-md-11 justify-content-center nav nav-pills\">\n" +
-  "            <li class=\"nav-item\"><a href=\"#\" onmouseenter=\"active_from_mouse(this)\" onmouseleave=\"inactive_from_mouse(this)\"\n" +
+  "            <li class=\"nav-item\"><a href=\"\" onmouseenter=\"active_from_mouse(this)\" onmouseleave=\"inactive_from_mouse(this)\"\n" +
   "                                    class=\"header-button nav-link\" aria-current=\"page\">Главная</a></li>\n" +
-  "            <li class=\"nav-item\"><a href=\"#\" onmouseenter=\"active_from_mouse(this)\" onmouseleave=\"inactive_from_mouse(this)\"\n" +
+  "            <li class=\"nav-item\"><a href=\"../html/collaborations.html\" onmouseenter=\"active_from_mouse(this)\" onmouseleave=\"inactive_from_mouse(this)\"\n" +
   "                                    class=\"header-button nav-link\" aria-current=\"page\">Участники</a></li>\n" +
-  "            <li class=\"nav-item\"><a href=\"#\" onmouseenter=\"active_from_mouse(this)\" onmouseleave=\"inactive_from_mouse(this)\"\n" +
+  "            <li class=\"nav-item\"><a href=\"../html/galary.html\" onmouseenter=\"active_from_mouse(this)\" onmouseleave=\"inactive_from_mouse(this)\"\n" +
   "                                    class=\"header-button nav-link\" aria-current=\"page\">Журнал</a></li>\n" +
   "          </ul>\n" +
   "        </div>\n" +
--- site/js/navbar.js
+++ site/js/navbar.js
@@ -1,23 +1,23 @@
 document.write("<div class=\"col-md-1 d-flex flex-column flex-shrink-0 p-3 text-bg-dark align-items-start\" style=\"width: 200px;\">\n" +
   "  <ul class=\"nav nav-pills flex-column mb-auto\">\n" +
   "    <li class=\"nav-item\">\n" +
-  "      <a href=\"#\" class=\"nav-link\" aria-current=\"page\"\n" +
+  "      <a href=\"../html/raport.html\" class=\"nav-link\" aria-current=\"page\"\n" +
   "         onmouseenter=\"active_from_mouse(this)\"\n" +
   "         onmouseleave=\"inactive_from_mouse(this)\">\n" +
   "        <svg class=\"bi pe-none me-2\" width=\"16\" height=\"16\">\n" +
   "          <use xlink:href=\"#home\"></use>\n" +
   "        </svg>\n" +
-  "        Home\n" +
+  "        Отчёт\n" +
   "      </a>\n" +
   "    </li>\n" +
   "    <li>\n" +
-  "      <a href=\"#\" class=\"nav-link text-white\"\n" +
+  "      <a href=\"../html/galary.html\" class=\"nav-link text-white\"\n" +
   "         onmouseenter=\"active_from_mouse(this)\"\n" +
   "         onmouseleave=\"inactive_from_mouse(this)\">\n" +
   "        <svg class=\"bi pe-none me-2\" width=\"16\" height=\"16\">\n" +
   "          <use xlink:href=\"#speedometer2\"></use>\n" +
   "        </svg>\n" +
-  "        Dashboard\n" +
+  "        Галерея\n" +
   "      </a>\n" +
   "    </li>\n" +
   "    <li>\n" +
