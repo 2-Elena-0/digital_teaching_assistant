87e1a51dcb528196c815902185275ee7046d15fc
Elena <e.nowitskaya2017@yandex.ru>
2025-05-20 22:25:10 +0300

    штрихи

--- site/js/navbar.js
+++ site/js/navbar.js
@@ -1,11 +1,21 @@
 document.write("<div class=\"col-md-1 d-flex flex-column flex-shrink-0 p-3 text-bg-dark align-items-start\" style=\"width: 200px;\">\n" +
   "  <ul class=\"nav nav-pills flex-column mb-auto\">\n" +
   "    <li class=\"nav-item\">\n" +
-  "      <a href=\"../html/raport.html\" class=\"nav-link\" aria-current=\"page\"\n" +
+  "      <a href=\"../html/about.html\" class=\"nav-link\" aria-current=\"page\"\n" +
   "         onmouseenter=\"active_from_mouse(this)\"\n" +
   "         onmouseleave=\"inactive_from_mouse(this)\">\n" +
   "        <svg class=\"bi pe-none me-2\" width=\"16\" height=\"16\">\n" +
   "          <use xlink:href=\"#home\"></use>\n" +
+  "        </svg>\n" +
+  "        О проекте\n" +
+  "      </a>\n" +
+  "    </li>\n" +
+  "    <li>\n" +
+  "      <a href=\"../html/raport.html\" class=\"nav-link text-white\"\n" +
+  "         onmouseenter=\"active_from_mouse(this)\"\n" +
+  "         onmouseleave=\"inactive_from_mouse(this)\">\n" +
+  "        <svg class=\"bi pe-none me-2\" width=\"16\" height=\"16\">\n" +
+  "          <use xlink:href=\"#speedometer2\"></use>\n" +
   "        </svg>\n" +
   "        Отчёт\n" +
   "      </a>\n" +
@@ -15,7 +25,7 @@ document.write("<div class=\"col-md-1 d-flex flex-column flex-shrink-0 p-3 text-
   "         onmouseenter=\"active_from_mouse(this)\"\n" +
   "         onmouseleave=\"inactive_from_mouse(this)\">\n" +
   "        <svg class=\"bi pe-none me-2\" width=\"16\" height=\"16\">\n" +
-  "          <use xlink:href=\"#speedometer2\"></use>\n" +
+  "          <use xlink:href=\"#table\"></use>\n" +
   "        </svg>\n" +
   "        Галерея\n" +
   "      </a>\n" +
@@ -25,47 +35,21 @@ document.write("<div class=\"col-md-1 d-flex flex-column flex-shrink-0 p-3 text-
   "         onmouseenter=\"active_from_mouse(this)\"\n" +
   "         onmouseleave=\"inactive_from_mouse(this)\">\n" +
   "        <svg class=\"bi pe-none me-2\" width=\"16\" height=\"16\">\n" +
-  "          <use xlink:href=\"#table\"></use>\n" +
+  "          <use xlink:href=\"#grid\"></use>\n" +
   "        </svg>\n" +
   "        Предложение\n" +
   "      </a>\n" +
   "    </li>\n" +
   "    <li>\n" +
-  "      <a href=\"#\" class=\"nav-link text-white\"\n" +
-  "         onmouseenter=\"active_from_mouse(this)\"\n" +
-  "         onmouseleave=\"inactive_from_mouse(this)\">\n" +
-  "        <svg class=\"bi pe-none me-2\" width=\"16\" height=\"16\">\n" +
-  "          <use xlink:href=\"#grid\"></use>\n" +
-  "        </svg>\n" +
-  "        Products\n" +
-  "      </a>\n" +
-  "    </li>\n" +
-  "    <li>\n" +
-  "      <a href=\"#\" class=\"nav-link text-white\"\n" +
+  "      <a href=\"../html/resources.html\" class=\"nav-link text-white\"\n" +
   "         onmouseenter=\"active_from_mouse(this)\"\n" +
   "         onmouseleave=\"inactive_from_mouse(this)\">\n" +
   "        <svg class=\"bi pe-none me-2\" width=\"16\" height=\"16\">\n" +
   "          <use xlink:href=\"#people-circle\"></use>\n" +
   "        </svg>\n" +
-  "        Customers\n" +
+  "        Ссылки\n" +
   "      </a>\n" +
   "    </li>\n" +
   "  </ul>\n" +
   "  <hr>\n" +
-  "  <div class=\"dropdown\">\n" +
-  "    <a href=\"#\" class=\"d-flex align-items-center text-white text-decoration-none dropdown-toggle\"\n" +
-  "       data-bs-toggle=\"dropdown\" aria-expanded=\"false\">\n" +
-  "      <img src=\"https://github.com/mdo.png\" alt=\"\" width=\"32\" height=\"32\" class=\"rounded-circle me-2\">\n" +
-  "      <strong>mdo</strong>\n" +
-  "    </a>\n" +
-  "    <ul class=\"dropdown-menu dropdown-menu-dark text-small shadow\" style=\"\">\n" +
-  "      <li><a class=\"dropdown-item\" href=\"#\">New project...</a></li>\n" +
-  "      <li><a class=\"dropdown-item\" href=\"#\">Settings</a></li>\n" +
-  "      <li><a class=\"dropdown-item\" href=\"#\">Profile</a></li>\n" +
-  "      <li>\n" +
-  "        <hr class=\"dropdown-divider\">\n" +
-  "      </li>\n" +
-  "      <li><a class=\"dropdown-item\" href=\"#\">Sign out</a></li>\n" +
-  "    </ul>\n" +
-  "  </div>\n" +
   "</div>")
